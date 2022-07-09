8de711e4b9640b63551268d006c400542bada9b9
Brandon Bahret <brandonbahret@gmail.com>
2022-07-09 02:43:41 -0400

    rebuilt website entirely

--- readme.md
+++ readme.md
@@ -1,4 +1,2 @@
 ## Welcome to the repository for my personal portfolio
 ### work in progress...
-
-The website was created entirely with vanilla html5 and css3. 
---
+++ scripts/css-helpers.js
@@ -0,0 +1,54 @@
+let root = document.documentElement;
+let primaryNav = document.querySelector(".primary-navigation");
+let menuToggle = document.querySelector(".menu-toggle");
+
+let bpPhone = 20; //rem
+let bpTablet = 39; //rem
+let bpMonitor = 57; //rem
+let bpJumbo = 80; //rem
+
+
+
+function closeNavigation() {
+    menuToggle.setAttribute('aria-expanded', false);
+    primaryNav.setAttribute('data-visible', false);
+}
+function openNavigation() {
+    menuToggle.setAttribute('aria-expanded', true);
+    primaryNav.setAttribute('data-visible', true);
+    primaryNav.setAttribute('data-animate', true);
+}
+function toggleNavigation() {
+    let isMenuExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
+    if (isMenuExpanded) {
+        closeNavigation();
+    } else {
+        openNavigation();
+    }
+}
+
+menuToggle.addEventListener("click", toggleNavigation);
+primaryNav.querySelectorAll("li").forEach(e => {
+    e.addEventListener("click", closeNavigation);
+});
+
+
+function onClientResize() {
+    let rootStyles = window.getComputedStyle(root);
+    let rootFontSize = parseInt(rootStyles.getPropertyValue('font-size'));
+    
+    let clientWidthInPixels = root.clientWidth;
+    let clientWidthInRems = clientWidthInPixels / rootFontSize;
+    
+    root.style.setProperty('--client-width', clientWidthInRems);
+
+    if (clientWidthInRems > bpTablet) {
+        closeNavigation();
+        primaryNav.setAttribute('data-animate', false);
+    }
+}
+
+
+onClientResize();
+window.addEventListener("resize", onClientResize);
+
