bd75a79aa53f34b3fe040cb87974d99056dbfc7d
Brandon Bahret <brandonbahret@gmail.com>
2022-07-11 04:47:01 -0400

    implemented accordion component

---
+++ scripts/accordion.js
@@ -0,0 +1,34 @@
+let accordions = document.querySelectorAll('.accordion');
+
+function collapseAccordion(accordion) {
+    accordion.querySelectorAll(".accordion-item").forEach(item => {
+        item.setAttribute('aria-expanded', false);
+    });
+}
+
+accordions.forEach(accordion => {
+    let items = accordion.querySelectorAll(".accordion-item");
+    
+    items.forEach(item => {
+        let aiHint = item.querySelector(".ai-hint");
+
+        aiHint.tabIndex = 1;
+        aiHint.addEventListener("keydown", e => {
+            if (e.code === "Enter" || e.code === "Space") {
+                e.preventDefault();
+                aiHint.click();
+            }
+        });
+
+        aiHint.addEventListener("click", e => {
+            let isExpanded = item.getAttribute('aria-expanded') === "true";
+
+            if (isExpanded) {
+                item.setAttribute('aria-expanded', false);
+            } else {
+                collapseAccordion(accordion);
+                item.setAttribute('aria-expanded', true);
+            }
+        });
+    });
+});
