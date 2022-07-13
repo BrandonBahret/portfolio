6b25c8cb5cd3664185e9a1f4200b2b9aae1b4bd2
Brandon Bahret <brandonbahret@gmail.com>
2022-07-13 03:32:57 -0400

    added basic client-side field validation to contact form

---
+++ scripts/contact-form.js
@@ -0,0 +1,42 @@
+let submitButton = document.querySelector("#contact-form button[type='submit']");
+let emailInput = document.querySelector("#email");
+let messageInput = document.querySelector("#message");
+let textFields = [emailInput, messageInput];
+
+
+function isEmpty(formElement) {
+    return formElement.value.trim().length == 0;
+}
+
+function isValidEmail(formElement) {
+    if (isEmpty(emailInput)) {
+        return false;
+    }
+
+    const regex = /[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\\.]+/gm;
+
+    let text = formElement.value.trim();
+    return (regex.exec(text)) !== null;
+}
+
+
+submitButton.addEventListener("click", e => {
+    if (isEmpty(messageInput)) {
+        messageInput.classList.add("bg-error");
+        e.preventDefault();
+    }
+
+    if (isValidEmail(emailInput) == false) {
+        emailInput.classList.add("bg-error");
+        e.preventDefault();
+    }
+
+    return true;
+});
+
+textFields.forEach(field => {
+    field.addEventListener("focus", () => {
+        field.classList.remove("bg-error");
+    });
+});
+
